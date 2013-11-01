3e0e75dc3d9a7b9e7d5f26da7c9d85566d403190
maaviles <macyaviles@MM.local>
2013-11-01 16:28:13 -0700

    Initial Commit

---
+++ HW3BubblesBubblesBubblesMyBubbles/MACAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MACAppDelegate.h
+//  HW3BubblesBubblesBubblesMyBubbles
+//
+//  Created by Macy Aviles on 11/1/13.
+//  Copyright (c) 2013 Macy Aviles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MACAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HW3BubblesBubblesBubblesMyBubbles/MACViewController.h
@@ -0,0 +1,13 @@
+//
+//  MACViewController.h
+//  HW3BubblesBubblesBubblesMyBubbles
+//
+//  Created by Macy Aviles on 11/1/13.
+//  Copyright (c) 2013 Macy Aviles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MACViewController : UIViewController
+
+@end
