0edcee1094bb2e0779d6c3d9fddbf6ec613724d5
maaviles <macyaviles@MM.local>
2013-11-04 17:08:47 -0800

    HW4 5 bubbles. falls and shrinks. click to pop. I want to add sound when click to pop in the future. Also add more grav control and add more bubble function

--- HW3BubblesBubblesBubblesMyBubbles/MACViewController.h
+++ HW3BubblesBubblesBubblesMyBubbles/MACViewController.h
@@ -10,4 +10,13 @@
 
 @interface MACViewController : UIViewController
 
+@property (nonatomic, strong) IBOutletCollection(UIView) NSArray *bubbleViews;
+@property (nonatomic, strong) IBOutletCollection(UIView) NSArray *childBubbleViews;
+
+@property (strong, nonatomic) IBOutlet UIView *bubble1;
+@property (strong, nonatomic) IBOutlet UIView *bubble2;
+@property (strong, nonatomic) IBOutlet UIView *bubble3;
+@property (strong, nonatomic) IBOutlet UIView *bubble4;
+@property (strong, nonatomic) IBOutlet UIView *bubble5;
+
 @end
--- HW3BubblesBubblesBubblesMyBubbles/MACViewController.m
+++ HW3BubblesBubblesBubblesMyBubbles/MACViewController.m
@@ -10,6 +10,8 @@
 
 @interface MACViewController ()
 
+@property (nonatomic, strong) UIDynamicAnimator *animator;
+
 @end
 
 @implementation MACViewController
@@ -17,13 +19,159 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    UITapGestureRecognizer *tapRecognizer1 = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTap1:)];
+    tapRecognizer1.numberOfTapsRequired = 1;
+    UITapGestureRecognizer *tapRecognizer2 = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTap2:)];
+    tapRecognizer2.numberOfTapsRequired = 1;
+    UITapGestureRecognizer *tapRecognizer3 = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTap3:)];
+    tapRecognizer3.numberOfTapsRequired = 1;
+    UITapGestureRecognizer *tapRecognizer4 = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTap4:)];
+    tapRecognizer4.numberOfTapsRequired = 1;
+    UITapGestureRecognizer *tapRecognizer5 = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTap5:)];
+    tapRecognizer5.numberOfTapsRequired = 1;
+    
+    
+    _animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
+    
+    //start gravity
+    UIGravityBehavior *gravityBehavior = [[UIGravityBehavior alloc] initWithItems:self.bubbleViews];
+    UIGravityBehavior *gravityChildBehavior = [[UIGravityBehavior alloc]
+                                               initWithItems:self.childBubbleViews];
+    [_animator addBehavior: gravityBehavior];
+    [_animator addBehavior: gravityChildBehavior];
+    
+    //start collision
+    UICollisionBehavior *collisionBehavior = [[UICollisionBehavior alloc] init];
+    collisionBehavior.translatesReferenceBoundsIntoBoundary = YES;
+    [_animator addBehavior: collisionBehavior];
+
+    
+    for (UIView *childBubbleView in _childBubbleViews)
+    {
+        {
+            UIView *view = [[UIView alloc] initWithFrame:CGRectMake(10, 10, 10, 10)];
+            [childBubbleView addSubview: view];
+            
+            childBubbleView.layer.cornerRadius = 25.f;
+            childBubbleView.clipsToBounds = YES;
+            
+            [gravityBehavior addItem: view];
+            [collisionBehavior addItem: view];
+
+        }
+        
+    }
+ 
+    for (UIView *bubbleView in self.bubbleViews)
+    {
+        {
+            
+            CABasicAnimation* boundsAnim = [CABasicAnimation animationWithKeyPath:@"bounds"];
+            boundsAnim.fromValue = [NSValue valueWithCGRect:bubbleView.layer.bounds];
+            boundsAnim.toValue = [NSValue valueWithCGRect:CGRectMake(10, 10, 50, 50)];
+            boundsAnim.duration = 3.0;
+            
+            [bubbleView.layer addAnimation:boundsAnim forKey:@"bounds"];
+            bubbleView.layer.bounds = CGRectMake(10, 10, 50, 50);
+            bubbleView.layer.cornerRadius = 25.f;
+            bubbleView.clipsToBounds = YES;
+            
+            [gravityBehavior addItem: bubbleView];
+            [collisionBehavior addItem: bubbleView];
+            
+            [_bubble1 addGestureRecognizer:tapRecognizer1];
+            [_bubble2 addGestureRecognizer:tapRecognizer2];
+            [_bubble3 addGestureRecognizer:tapRecognizer3];
+            [_bubble4 addGestureRecognizer:tapRecognizer4];
+            [_bubble5 addGestureRecognizer:tapRecognizer5];
+            
+        }
+        
+    }
+    
+}
+
+- (void)handleTap1:(UITapGestureRecognizer *)sender
+{
+    if (sender.state == UIGestureRecognizerStateEnded)
+    {
+        CABasicAnimation* boundsAnim = [CABasicAnimation animationWithKeyPath:@"bounds"];
+        boundsAnim.fromValue = [NSValue valueWithCGRect:_bubble1.layer.bounds];
+        boundsAnim.toValue = [NSValue valueWithCGRect:CGRectMake(10, 10, 30, 30)];
+        boundsAnim.duration = 0.2;
+        [_bubble1.layer addAnimation:boundsAnim forKey:@"bounds"];
+        
+        _bubble1.layer.bounds = CGRectMake(10, 10, 0, 0);
+    }
+}
+
+- (void)handleTap2:(UITapGestureRecognizer *)sender
+{
+    if (sender.state == UIGestureRecognizerStateEnded)
+    {
+        CABasicAnimation* boundsAnim = [CABasicAnimation animationWithKeyPath:@"bounds"];
+        boundsAnim.fromValue = [NSValue valueWithCGRect:_bubble2.layer.bounds];
+        boundsAnim.toValue = [NSValue valueWithCGRect:CGRectMake(10, 10, 30, 30)];
+        boundsAnim.duration = 0.2;
+        [_bubble2.layer addAnimation:boundsAnim forKey:@"bounds"];
+        
+        _bubble2.layer.bounds = CGRectMake(10, 10, 0, 0);
+    }
+}
+
+- (void)handleTap3:(UITapGestureRecognizer *)sender
+{
+    if (sender.state == UIGestureRecognizerStateEnded)
+    {
+        CABasicAnimation* boundsAnim = [CABasicAnimation animationWithKeyPath:@"bounds"];
+        boundsAnim.fromValue = [NSValue valueWithCGRect:_bubble3.layer.bounds];
+        boundsAnim.toValue = [NSValue valueWithCGRect:CGRectMake(10, 10, 30, 30)];
+        boundsAnim.duration = 0.2;
+        [_bubble3.layer addAnimation:boundsAnim forKey:@"bounds"];
+        
+        _bubble3.layer.bounds = CGRectMake(10, 10, 0, 0);
+    }
+}
+
+- (void)handleTap4:(UITapGestureRecognizer *)sender
+{
+    if (sender.state == UIGestureRecognizerStateEnded)
+    {
+        CABasicAnimation* boundsAnim = [CABasicAnimation animationWithKeyPath:@"bounds"];
+        boundsAnim.fromValue = [NSValue valueWithCGRect:_bubble4.layer.bounds];
+        boundsAnim.toValue = [NSValue valueWithCGRect:CGRectMake(10, 10, 30, 30)];
+        boundsAnim.duration = 0.2;
+        [_bubble4.layer addAnimation:boundsAnim forKey:@"bounds"];
+        
+        _bubble4.layer.bounds = CGRectMake(10, 10, 0, 0);
+    }
+}
+
+- (void)handleTap5:(UITapGestureRecognizer *)sender
+{
+    if (sender.state == UIGestureRecognizerStateEnded)
+    {
+        CABasicAnimation* boundsAnim = [CABasicAnimation animationWithKeyPath:@"bounds"];
+        boundsAnim.fromValue = [NSValue valueWithCGRect:_bubble5.layer.bounds];
+        boundsAnim.toValue = [NSValue valueWithCGRect:CGRectMake(10, 10, 30, 30)];
+        boundsAnim.duration = 0.2;
+        [_bubble5.layer addAnimation:boundsAnim forKey:@"bounds"];
+        
+        _bubble5.layer.bounds = CGRectMake(10, 10, 0, 0);
+    }
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
 }
 
 @end
